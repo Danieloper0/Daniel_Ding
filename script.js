8c7ef5889266d7d8806e5940b0e15cd63426b2ab
Danieloper0 <165540356+Danieloper0@users.noreply.github.com>
2024-08-12 17:24:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.querySelector("button").addEventListener("click", function() {
     const content = document.querySelector(".content");
-
     content.classList.add("hidden");
-
+    
     setTimeout(() => {
-        document.body.style.backgroundColor = "#add8e6"; 
-    }, 500); 
+        document.body.style.backgroundColor = "#add8e6";
+    }, 500);
 });
