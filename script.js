8c84793bb99757402015241a8e533bc971a792c9
Daniel <165540356+Danieloper0@users.noreply.github.com>
2024-08-24 21:13:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Toggle Menu and Button Animation
-document.getElementById('menuButton').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    var menuButton = document.getElementById('menuButton');
-    
-    menu.classList.toggle('active');
-    menuButton.classList.toggle('tilt');
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    this.classList.toggle('open');
+    document.getElementById('slide-menu').classList.toggle('open');
+});
+
+document.getElementById('title').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent opening in a new tab
+    window.location.href = 'index.html';
 });
