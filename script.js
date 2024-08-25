cda2165b6a1166139c5596da736669e566ffd69d
Daniel <165540356+Danieloper0@users.noreply.github.com>
2024-08-24 21:06:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// JavaScript to trigger the floating animation when the page loads
-window.addEventListener('load', function() {
-    var floatingImage = document.getElementById('floatingImage');
-    floatingImage.style.animationDelay = '0.5s'; // Adjust delay for when the image should start floating
+// Toggle Menu and Button Animation
+document.getElementById('menuButton').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    var menuButton = document.getElementById('menuButton');
+    
+    menu.classList.toggle('active');
+    menuButton.classList.toggle('tilt');
 });
