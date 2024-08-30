63cc99e7961983891d44d69d4220f42feb9ad800
Daniel <165540356+Danieloper0@users.noreply.github.com>
2024-08-29 21:55:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 window.onload = function() {
     const textElement = document.getElementById("typing-text");
 
-    let index = 0;
-    const text = "Welcome to My World\nProfessional Violinist, Aspiring Programmer, and Chess enthusiast";
-    
-    function type() {
-        if (index < text.length) {
-            textElement.innerHTML += text[index] === "\n" ? "<br>" : text[index];
-            index++;
-            setTimeout(type, 100); // Adjust typing speed here
-        } else {
-            textElement.style.borderRight = "none"; // Hide cursor once typing is done
+    if (textElement) {
+        let index = 0;
+        const text = "Welcome to My World\nProfessional Violinist, Aspiring Programmer, and Chess enthusiast";
+        
+        function type() {
+            if (index < text.length) {
+                textElement.innerHTML += text[index] === "\n" ? "<br>" : text[index];
+                index++;
+                setTimeout(type, 100);
+            } else {
+                textElement.style.borderRight = "none";
+            }
         }
-    }
 
-    type();
+        type();
+    } else {
+        console.error("Element with id 'typing-text' not found.");
+    }
 };
